046114d6f97ba63527a7f4097afcfc05eaa7d1bb
ANTARIKSH2007TAMULY <tamulyantariksh601@gmail.com>
2026-02-25 11:13:13 +0530

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-  base: '/Find-Blood-Donor/', // ✅ important for GitHub Pages
-  plugins: [react(), tailwindcss()],
-})
